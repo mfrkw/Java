77ed0ffcb826e22e24043fa37332bce204efd822
mfrkw <xjdk3766@gmail.com>
2021-09-30 13:35:37 +0000

    [配列]

---
+++ practice/task1.java
@@ -0,0 +1,15 @@
+class task1 {
+	public static void main (String[] args) {
+	  
+	  String[] languages = {"Ruby", "PHP", "Python"};
+    
+    
+    System.out.println(languages[1]);
+    
+    
+    languages[1] ="Java";
+    
+    // インデックス番号が「1」の要素を出力してください
+    System.out.println(languages[1]); 
+	}
+}
