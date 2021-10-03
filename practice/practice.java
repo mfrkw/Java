f32597249c6d5960f44366a404904da5220ac9ae
mfrkw <xjdk3766@gmail.com>
2021-10-03 14:28:34 +0000

    [for文]

--- practice/practice.java
+++ practice/practice.java
@@ -1,5 +1,12 @@
 class practice {
-  public static void main (String args[]){
-    System.out.println("Hello World");
+  public static void main(String[] args) {
+    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
+    
+    // for文を用いて、「私の名前は◯◯です」と出力してください
+    for(int i = 0; i< names.length; i ++){
+      System.out.println("私の名前は" + names[i]+ "です");
+    }
+    
   }
 }
+
