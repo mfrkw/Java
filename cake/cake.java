80fee5d0144f37e6c99d4283e5760ffca703c809
mfrkw <xjdk3766@gmail.com>
2021-10-13 11:47:51 +0000

    [変数]

--- cake/cake.java
+++ cake/cake.java
@@ -1,9 +1,17 @@
-class cake {
+//コマンドライン　商品名　個数　商品名　個数
+//                 [0]     [1]    [2]    [3]
+
+
+
+
+//必要な変数
+//ケーキの名前　ケーキの個数　合計金額　　支払金額  消費税　割引率　
+class cake {　
   public static void main (String[] args) {
     
-    int 
-    int amountSum =
     
+    int totalPrice = 0;
+    int pay = 
     
     
     
