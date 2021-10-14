f2dbbc4487d711a0220a1c26198ba03fae45e1b0
mfrkw <xjdk3766@gmail.com>
2021-10-14 09:55:21 +0000

    [入力]

--- instruction/Practice.java
+++ instruction/Practice.java
@@ -1,9 +1,13 @@
 public class Practice {
   public static void main (String [] args){
     
-    int r = new java.util.Random().nextInt(90);
+    System.out.println("貴方の名前を入力して下さい");
     
-    System.out.println("貴方は" + r + "歳ですね？");
+    String name = new java.util.Scanner(System.in).nextLine();
+    
+    int r = new java.util.Random().nextInt(90);   // nextInt(90)で指定することで　0<90の数値をランダムに取る
+    
+    System.out.println("ようこそ" + name + "さん" + "貴方は" + r + "歳ですね？");
     
   }
 }
