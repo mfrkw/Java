b578b6c60b5a7ab645e09720fe4ff9178aad9d03
mfrkw <xjdk3766@gmail.com>
2021-10-18 12:29:16 +0000

    [task9]

---
+++ task/Task9.java
@@ -0,0 +1,20 @@
+public class Task9 {
+  public static void main (String[] args){
+    
+    introduceOneself();
+    
+  }
+  public static void introduceOneself() {
+    
+    String name = "美沙樹";
+    int age = 24;
+    double height = 150.1;
+    char earthlyBranches = '丑';
+    
+    System.out.println("私の名前は" + name);
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("身長は" + height + "cm" + "で干支は" + earthlyBranches + "です");
+
+
+  }
+}
